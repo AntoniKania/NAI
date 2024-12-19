0fd67636499d32e982e3a4e7eff7ecb328c11523
Rafał Sojecki <s26286@pjwstk.edu.pl>
2024-12-19 20:54:00 +0100

    Readme, show classes, cat's bread

--- 05-neural-networks/README.md
+++ 05-neural-networks/README.md
@@ -1,4 +1,46 @@
-## Accuracy comparison with decision tree and SVM for pima dataset
+# Neural networks
+
+Authors: Antoni Kania, Rafał Sojecki
+
+The application provides an implementation of neural networks for solving classification problems.
+
+## Usage
+
+1. Install the dependencies listed in the `requirements.txt` file (in root folder of repository):
+    ```bash
+    pip install -r requirements.txt
+    ```
+2. Run application
+   ```bash
+    python index.py <model_name>
+    ```
+
+Application will run all functionality described in [models](Models) for each dataset in order.
+
+## Models
+
+### fashion_mnist
+
+### cats
+
+On this network we compared ussage of 2 sizes of neural networks (by neuron count)
+
+#### 16 neurons
+
+![16](media/16.png)
+
+#### 256 neurons
+
+![256](media/256.png)
+
+Bigger neural networks have better accuracy than smaller ones, but the learning process takes longer. In both cases, the network correctly classified this cat.
+
+ ![256](resources/random.jpg)
+### cifar10
+ ![cifar10](media/cifar10.png)
+### pima
+
+#### Accuracy comparison with decision tree and SVM for pima dataset
 - Neural Network Accuracy: 0.7494
 - Decision Tree Accuracy: 0.7077
 - SVM Accuracy: 0.7597
@@ -8,3 +50,4 @@
 ![decision_tree](media/accuracy_pima_decision_tree.png)
 
 ![decision_tree](media/accuracy_pima_svm.png)
+
---
+++ 05-neural-networks/index.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import sys
+import numpy as np
+
+from datasets import fashion_mnist, pima, cifar10, cats_breeds
+from confusion_matrix import print_confusion_matrix
+from models import get_trained_pima_model, get_trained_cifar10_model, get_trained_mnist_model, get_trained_cats_breeds_model
+from classifiers import invoke_pima_model_with_sample_data, invoke_cifar10_model_with_sample_data, invoke_cats_model_with_sample_data
+from collections import Counter
+
+if __name__ == '__main__':
+    dataset_type = sys.argv[1]
+    model_filename = f'{dataset_type}_model.keras'
+
+    if dataset_type == 'fashion_mnist':
+        (X_train, y_train), (X_test, y_test), input_shape, num_classes = fashion_mnist()
+        model = get_trained_mnist_model(input_shape, num_classes, X_train, y_train)
+        model.save(model_filename)
+        model.evaluate(X_test, y_test)
+
+    elif dataset_type == 'cats':
+        (X_train, y_train), (X_test, y_test), input_shape, num_classes = cats_breeds()
+        print(Counter(np.argmax(y_train, axis=1)))
+        model = get_trained_cats_breeds_model(input_shape, num_classes, X_train, y_train)
+        model.save(model_filename)
+        model.evaluate(X_test, y_test)
+        invoke_cats_model_with_sample_data(model)
+
+    elif dataset_type == 'pima':
+        (X_train, y_train), (X_test, y_test), input_shape, num_classes = pima()
+        model = get_trained_pima_model(input_shape, num_classes, X_train, y_train)
+        model.save(model_filename)
+        model.evaluate(X_test, y_test)
+        print_confusion_matrix(model, X_test, y_test, num_classes)
+        invoke_pima_model_with_sample_data(model)
+        
+    elif dataset_type == 'cifar10':
+        (X_train, y_train), (X_test, y_test), input_shape, num_classes = cifar10()
+        model = get_trained_cifar10_model(input_shape, num_classes, X_train, y_train)
+        model.save(model_filename)
+        model.evaluate(X_test, y_test)
+        invoke_cifar10_model_with_sample_data(model, X_test[0:1])
+    else:
+        raise ValueError("Unknown dataset type. Use 'fashion_mnist', 'pima', or 'cifar10'.")
+
+    print(f"Model training and evaluation completed for {dataset_type} dataset.")
